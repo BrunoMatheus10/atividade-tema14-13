bbd4d50a0e8845106ec7d2b773c1abfb6f7f84e1
BrunoMatheus10 <brunomatheus88300432@gmail.com>
2024-09-19 01:09:35 -0300

    Add files via upload

---
+++ atividade-tema14-13.py
@@ -0,0 +1,14 @@
+# Inicialização das variáveis
+A = 10
+B = 20
+
+# Exibição dos valores iniciais
+print("Valores iniciais: A = ",A," B = ",B)
+
+# Troca dos valores utilizando uma variável temporária
+temp = A
+A = B
+B = temp
+
+# Exibição dos valores após a troca
+print("Valores após a troca: A = ",A," B = ",B)
